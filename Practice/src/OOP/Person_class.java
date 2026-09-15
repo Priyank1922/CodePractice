61d36cd0366469a8966266ba9f2785e5af299c6c
Priyank1922 <priyankmehta409@gmail.com>
2026-09-15 11:26:57 +0530

    class and object

---
+++ Practice/src/OOP/Person_class.java
@@ -0,0 +1,11 @@
+package OOP;
+
+public class Person_class{
+	int age = 20;
+
+	public static void main(String []args) {
+		
+		Person_class Ajay = new Person_class();
+		System.out.println(Ajay.age);
+	}
+}
---
+++ Practice/src/OOP/Student.java
@@ -0,0 +1,12 @@
+package OOP;
+
+public class Student {
+int rollno;
+String studentName;
+
+public static void main(String [] args) {
+	Student obj1 = new Student();
+	System.out.println(obj1.rollno);
+	System.out.println(obj1.studentName);
+}
+}
