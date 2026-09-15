1e776d44b2a453008bc037ce9871280e2d8ba67e
Priyank1922 <priyankmehta409@gmail.com>
2026-09-15 11:35:56 +0530

    Update;

--- Practice/src/OOP/Student.java
+++ Practice/src/OOP/Student.java
@@ -1,12 +1,22 @@
 package OOP;
 
 public class Student {
-int rollno;
-String studentName;
+	int rollno;
+	String studentName;
 
-public static void main(String [] args) {
-	Student obj1 = new Student();
-	System.out.println(obj1.rollno);
-	System.out.println(obj1.studentName);
-}
+	public static void main(String[] args) {
+		Student obj1 = new Student();
+		obj1.rollno = 1;
+		obj1.studentName = "Nitesh";
+		System.out.println(obj1.rollno);
+		System.out.println(obj1.studentName);
+
+		System.out.println("=========================================");
+
+		Student obj2 = new Student();
+		obj2.rollno = 2;
+		obj2.studentName = "Ritesh";
+		System.out.println(obj2.rollno);
+		System.out.println(obj2.studentName);
+	}
 }
